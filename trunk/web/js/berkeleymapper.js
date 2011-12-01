3add9110981c90f99c956894ce511b27457f33fe
jdeck88@gmail.com <jdeck88@gmail.com@e64f2171-5026-4201-70ad-b28c1de443fa>
2011-12-01 02:12:39 +0000

    Google maps chosen over openlayers due to more available features and familiarity with API. Removed OL stuff Added drawingManager and created event listeners to support BerkeleyMapper type functionality. More elegant method for opening/closing left panel (to show attributes)

--- trunk/web/js/berkeleymapper.js
+++ trunk/web/js/berkeleymapper.js
@@ -1,64 +1,175 @@
 
-
+var map;
+//var poly, map;
+//var polyPath = new google.maps.MVCArray;
+//var polyMarkers = [];
+var overlays = []; // array of user-defined overlays
+var overlayMarkers = []; // array of markers for user-defined overlays
 
 function initialize() {
     
     // Initial State
-$("#leftnav").hide();
+    $("#leftnav").hide();
 
-    var map = getMap();
-    //setKML(getKMLFileName(jQuery.url),map);
+    map = getMap();
+    
+    map.enableKeyDragZoom({
+        visualEnabled: true,
+        visualPosition: google.maps.ControlPosition.LEFT,
+        visualPositionOffset: new google.maps.Size(35, 0),
+        visualPositionIndex: null,
+        visualSprite: "http://maps.gstatic.com/mapfiles/ftr/controls/dragzoom_btn.png",
+        visualSize: new google.maps.Size(20, 20),
+        visualTips: {
+            off: "Turn on",
+            on: "Turn off"
+        }
+    });
+    // This is the KML Point file
     setKML('http://darwin.berkeley.edu/kmltest2.kml', map);
+
+    // Setup Map type Options (add KML overlays to this??)
     setMapTypes(map);   
     
-    // Attributes Control
-    var attControl = new NewControl(document.createElement('DIV'), map,'Attributes','Click to Toggle Attributes Visibility');
+    // Control Panel pops over from left side
+    var attControl = new PanelControl(document.createElement('DIV'), map);
     google.maps.event.addDomListener(attControl, 'click', function() {
         $("#leftnav").toggle("slide", {
             direction: "left"
         }, 1000, function() {
-            google.maps.event.trigger(map, "resize");
-
-        });
-              
-    });
-  
-    // Options Control
-    //var resControl = new NewControl(document.createElement('DIV'), map,'Resize','Click to Manually Resize Map');
-   // google.maps.event.addDomListener(resControl, 'click', function() {
-   //     google.maps.event.trigger(map, "resize");
-   // });
-
-  
-//setControl("Services",map);
-//setControl("Points",map);
-//setControl("Layers",map);
-//setControl("Download",map);
-//setControl("Map",map);
-
-}
-
-// Set a Control, must be defined as a div tag on page
-/*
-function setControl(name, map) {
-    var customMapControls = document.createElement('div');
-    var customControlDiv = document.getElementById(name).innerHTML;
-    customMapControls.innerHTML = customControlDiv;
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(customMapControls);
-    google.maps.event.addDomListener(customMapControls, 'click', function() {        
-        //$('div.showhide,#'+name+'Box').show();
-        $('div.showhide,#slider').hide();
-    }); 
- */
+            google.maps.event.trigger(map, "resize");                        
+        });                     
+    });                   
     
-/*var customMapControls2 = document.createElement('div');
-    var customControlDiv2 = document.getElementById('button').innerHTML;
-    customMapControls2.innerHTML = customControlDiv2;
-     google.maps.event.addDomListener(document.getElementById('button'), 'click', function() {
-alert('foodad');
-  });
+    // Darwing Manager for Polygons and Circles
+    var drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: null,
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+            google.maps.drawing.OverlayType.CIRCLE,
+            google.maps.drawing.OverlayType.POLYLINE,
+            google.maps.drawing.OverlayType.POLYGON
+            ]
+        },       
+        polygonOptions: {
+            fillColor: '#5555FF',
+            fillOpacity: 0.5,
+            strokeWeight: 3,
+            clickable: true,
+            editable: false,
+            zIndex: 1
+        }
+    });
+    drawingManager.setMap(map);
+    
+    google.maps.event.addDomListener(drawingManager, 'polylinecomplete', function(l) {
+        overlays.push(l);
+        index = overlays.length - 1;
+        
+        var lengthInMeters = Math.round(google.maps.geometry.spherical.computeLength(l.getPath()));
+        var lengthInFeet = Math.round(lengthInMeters*3.2808399);
+        var lengthInMiles = (lengthInFeet/5280).toFixed(4);
+
+        var contentString = "<div id='content'>User Defined Polyline:" +
+        "<br>Length in Meters: <i> " + lengthInMeters + " </i>" +
+        "<br>Length in Feet: <i> " + lengthInFeet + " </i>" +
+        "<br>Length in Miles: <i> " + lengthInMiles + " </i>" +
+        "<br>---------------" +
+        "<br><a href='#' id='delete' onclick='removeOverlay(" + index  + ");'>Delete Shape</a>" +
+        "</div>";
+    
+        var marker = new google.maps.Marker({
+            position: l.getBounds().getCenter(), 
+            map: map,
+            title:"User Polyline"
+        }); 
+        var infowindow = new google.maps.InfoWindow({            
+            content: contentString
+        });
+            
+        infowindow.open(map,marker);
+        google.maps.event.addListener(marker, 'click', function() {            
+            infowindow.open(map,marker);
+        });
+        
+        drawingManager.setDrawingMode(null); // revert to normal map mode
+        
+        overlayMarkers.push(marker); 
+    });
+    
+    google.maps.event.addDomListener(drawingManager, 'circlecomplete', function(c) {
+        overlays.push(c);
+        index = overlays.length - 1;
+        var lat = c.getCenter().lat().toFixed(4);
+        var lng = c.getCenter().lng().toFixed(4);
+        var radius = Math.round(c.getRadius());
+        var contentString = "<div id='content'>User Defined Point & Error Radius:" +
+        "<br>Center: <i>" +lat +", " + lng + "</i>" +
+        "<br>Error Radius In Meters: <i>" + radius + "</i>" +
+        "<br>Text: <i>Generated visually in BerkeleyMapper</i>" + 
+        "<br>---------------" +
+        "<br><a href='#' id='callback' onclick='callbackPoint(" + index + ");'>Callback</a>" +
+        "<br><a href='#' id='delete' onclick='removeOverlay(" + index  + ");'>Delete Shape</a>" +
+        "</div>";
+    
+        var marker = new google.maps.Marker({
+            position: c.getBounds().getCenter(), 
+            map: map,
+            title:"User Point & Error Radius"
+        }); 
+        var infowindow = new google.maps.InfoWindow({            
+            content: contentString
+        });
+            
+        infowindow.open(map,marker);
+        google.maps.event.addListener(marker, 'click', function() {            
+            infowindow.open(map,marker);
+        });
+        
+        drawingManager.setDrawingMode(null); // revert to normal map mode
+        
+        overlayMarkers.push(marker); 
+    });
+        
+    google.maps.event.addDomListener(drawingManager, 'polygoncomplete', function(p) {        
+        overlays.push(p);        
+        index = overlays.length - 1;
+        
+        var areaInSqMeters = Math.round(google.maps.geometry.spherical.computeArea(p.getPath()));
+        var areaInAcres = Math.round(areaInSqMeters/4046.85642);
+        var areaInSqMiles = (areaInSqMeters*0.000000386102159).toFixed(4);
+                
+        var contentString = "<div id='content'>Polygon statistics:" + 
+        "<br>Sq Meters = " +areaInSqMeters  +
+        "<br>Acres = " + areaInAcres +
+        "<br>Sq Miles = " + areaInSqMiles +
+        "<br>---------------" +
+        "<br><a href='#' id='query' onclick='queryOverlay(" + index  + ");'>Query Points Inside</a>" +
+        "<br><a href='#' id='delete' onclick='removeOverlay(" + index  + ");'>Delete Shape</a>" +
+        "</div>";        
+
+        var marker = new google.maps.Marker({
+            position: p.getBounds().getCenter(), 
+            map: map,
+            title:"User Polygon"
+        }); 
+        var infowindow = new google.maps.InfoWindow({            
+            content: contentString
+        });
+            
+        infowindow.open(map,marker);
+        google.maps.event.addListener(marker, 'click', function() {            
+            infowindow.open(map,marker);
+        });
+        
+        drawingManager.setDrawingMode(null); // revert to normal map mode
+        
+        overlayMarkers.push(marker);        
+    });
+
 }
-*/
 
 // Construct the KMLFileName by calling the berkeleymapper service
 function getKMLFileName(url) {
@@ -71,6 +182,7 @@ function getKMLFileName(url) {
     return kmlfile;
 }
 
+
 // set the KML file
 function setKML(kmlfile, map) {
     var ptLayer = new google.maps.KmlLayer(kmlfile);
@@ -80,9 +192,16 @@ function setKML(kmlfile, map) {
 // Set the initial Map
 function getMap(map) {
     var myOptions = {
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,        
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        }
     };
-
     return new google.maps.Map(document.getElementById('map'), myOptions);
 }
 
@@ -118,12 +237,25 @@ function setMapTypes(map) {
     });
 }
 
+function PanelControl(controlDiv, map) {
+    //controlDiv.style.padding = '5px';
+    
+    controlDiv.index = -1;  // value of -1 supersedes control position of others
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(controlDiv);
+    var controlUI = document.createElement('DIV');
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.backgroundImage = 'url(img/left-right.gif)';
+    controlUI.title = 'Show/Hide Panel';
+    controlUI.style.width = '29px';
+    controlUI.style.height = '20px'; 
+    
+    controlDiv.appendChild(controlUI);
+    
+    return controlUI;
+}
 
 function NewControl(controlDiv, map, title, alt) {
-
-    // Set CSS styles for the DIV containing the control
-    // Setting padding to 5 px will offset the control
-    // from the edge of the map
+   
     controlDiv.style.padding = '5px';
     controlDiv.index = 1;
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv);
@@ -149,6 +281,43 @@ function NewControl(controlDiv, map, title, alt) {
     controlUI.appendChild(controlText);
   
     return controlUI;
+}
 
+if (!google.maps.Polygon.prototype.getBounds) {
+    google.maps.Polygon.prototype.getBounds = function(latLng) {
+        var bounds = new google.maps.LatLngBounds();
+        var paths = this.getPaths();
+        var path;
+                
+        for (var p = 0; p < paths.getLength(); p++) {
+            path = paths.getAt(p);
+            for (var i = 0; i < path.getLength(); i++) {
+                bounds.extend(path.getAt(i));
+            }
+        }
+        return bounds;
+    }
+}
 
+if (!google.maps.Polyline.prototype.getBounds) {
+    google.maps.Polyline.prototype.getBounds = function(latLng) {
+        var bounds = new google.maps.LatLngBounds();        
+        this.getPath().forEach(function(e) {
+            bounds.extend(e);
+        });
+        return bounds;                  
+    }
+}
+
+function removeOverlay(num) {
+    overlays[num].setMap(null);
+    overlayMarkers[num].setMap(null);
+}
+
+function queryOverlay(num) {
+    var encoded = google.maps.geometry.encoding.encodePath(overlays[num].getPath());
+    alert("send to server following encoding and query it: " + encoded );      
+}
+function callbackPoint(num) {
+    alert("possible to send this data back to some calling application??");      
 }
