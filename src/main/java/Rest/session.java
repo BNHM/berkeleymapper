3d50839a802c09d07b5ae2da5b8e6dcd12c07849
John Deck <jdeck88@gmail.com>
2020-02-04 10:45:35 -0800

    adding better debugging in session class

--- src/main/java/Rest/session.java
+++ src/main/java/Rest/session.java
@@ -45,6 +45,7 @@ public class session {
         } catch (Exception e) {
             configUrl = null;
             System.err.println(e.getMessage());
+            e.printStackTrace();
             rb = Response.status(204);
         }
 
@@ -54,6 +55,7 @@ public class session {
             BMConfigAndTabFileReader f = new BMConfigAndTabFileReader(url, configUrl);
             rb = Response.ok(f.getSession().getSessionString());
         } catch (IOException e) {
+            e.printStackTrace();
             rb = Response.status(204);
             return rb.build();
         }
@@ -77,18 +79,19 @@ public class session {
         } catch (Exception e) {
             configUrl = null;
             System.err.println(e.getMessage());
+            e.printStackTrace();
             rb = Response.status(204);
         }
 
         // Load the Tab File
         //URL url = new URL(tabdata);
         try {
-            BMConfigAndTabFileReader f = new BMConfigAndTabFileReader(tabdata, configUrl);
             System.out.println("tabdata=" + tabdata);
             System.out.println("configUrl = " + configUrl);
-
+            BMConfigAndTabFileReader f = new BMConfigAndTabFileReader(tabdata, configUrl);
             rb = Response.ok(f.getSession().getSessionString());
         } catch (IOException e) {
+            e.printStackTrace();
             rb = Response.status(204);
             return rb.build();
         }
