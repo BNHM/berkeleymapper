0897b05f08cf89597ad034982deb9ad6557c49b1
John Deck <jdeck88@gmail.com>
2022-11-21 15:23:34 -0800

    adding a deployment script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# instructions for development and deployment which is done on the command-line
+# first make sure we add localhost referrer in:
+# https://console.cloud.google.com/apis/credentials?project=berkeleymapper
+# add the following:
+# http://localhost:8080/*
+# make sure you disable this when done developing
+
+# build the war file
+./gradlew war
+# copy it into place on this machine
+cp dist/berkeleymapper.war /usr/local/Cellar/jetty/9.4.42.v20210604/libexec/webapps/root.war
+# open a test page
+/usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:8080/index.html?tabfile=https://raw.githubusercontent.com/BNHM/berkeleymapper/master/examples/ucjeps.txt&configfile=https://raw.githubusercontent.com/BNHM/berkeleymapper/master/examples/ucjeps.xml'
+
